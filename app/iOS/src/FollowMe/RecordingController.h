a6b11619966f379d699010964e17e2e963ebe012
Steve Yalovitser <syalovitser@yahoo.com>
2010-07-13 23:26:21 -0400

    added audio annotation

--- app/iOS/src/FollowMe/RecordingController.h
+++ app/iOS/src/FollowMe/RecordingController.h
@@ -7,9 +7,19 @@
 
 #import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
+#import <AudioToolbox/AudioToolbox.h>
+#import <AVFoundation/AVFoundation.h>
 
 @interface RecordingController : UIViewController {
-
+	NSMutableDictionary *recordSetting;
+	NSString * recorderFilePath;
+	AVAudioRecorder * recorder;
 }
+
+- (void) startRecording;
+- (void) stopRecording;
+
+- (IBAction)listenUp:(UIView *)sender;
+- (IBAction)listenDown:(UIView *)sender;
 
 @end
